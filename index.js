aa97cfe5c6d980995500869bff83a8248901ea76
goliatone <burgosemi@gmail.com>
2015-08-14 14:14:19 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var scanner = require('arpscan');
+
+var program = require('commander');
+
+var pck = require('./package.json');
+
+
+
+program
+    .version(pck.version)
+    .option('-m, --mac-address [macAddress]', 'MAC address to resolve')
+    // .option('-C, --check-arpscan', 'Test to see if arp-scan is installed', checkArpScan)
+    .parse(process.argv);
+
+var MAC = program.macAddress;
+if(!MAC) process.exit(0);
+
+MAC = MAC.toUpperCase();
+
+scanner(function result(err, data){
+    if(err){
+        process.stderr.write(err + '\n');
+    }
+
+    data = data || [];
+
+    data.map(function(item){
+        if(item.mac.toUpperCase() === MAC) process.stdout.write(item.ip);
+    })
+    // process.stdout.write(JSON.stringify(data) + '\n');
+});
